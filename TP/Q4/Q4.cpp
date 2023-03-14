49dea049a3776f55ffef1ce4934349060eabe2d2
nolancx7 <nolan.cacheux@student.junia.com>
2023-03-14 11:44:02 +0100

    FINI

--- TP/Q4/Q4.cpp
+++ TP/Q4/Q4.cpp
@@ -22,7 +22,6 @@ struct P2Data {
     vector<vector<int>> distances; // Matrice des distances
 };
 
-
 // Fonction pour lire les données du problème P1 à partir d'un fichier
 P1Data readP1File(std::string filename) {
     std::ifstream infile(filename);
@@ -96,65 +95,29 @@ void printP2Data(P2Data data) {
 }
 
 // Fonction pour résoudre le problème P1 avec un algorithme glouton
-vector<int> solveP1(P1Data& data, int seed) {
-    auto copie_data = data;
+vector<int> solveP1(P1Data& data) {
+
+    auto copie_data = data; //On fait une copie pour garder la structure non triée de data afin de ne pas nous perdre lors de l'affichage des indices
+
     // Tri des objets en fonction du ratio bénéfice/consommation décroissant
     sort(data.objects.begin(), data.objects.end(), [](auto& o1, auto& o2) {
         double ratio1 = (double)std::get<2>(o1) / (double)std::get<1>(o1);
     double ratio2 = (double)std::get<2>(o2) / (double)std::get<1>(o2);
     return ratio1 > ratio2;
         });
-
     // Variables pour le suivi de la solution
     int total_profit = 0;
     int total_consumption = 0;
     vector<int> solution(data.num_objects, 0);
-
     std::cout << "\nOn affiche les objets selon le meilleur rapport “bénéfice/capacité consommée : " << std::endl;
     printP1Data(data);
-
     // Boucle sur les objets triés
     for (int i = 0; i < data.num_objects; i++) {
         // Vérification si l'objet peut être inclus dans la solution
-        if ((total_consumption + std::get<1>(data.objects[i]) <= data.capacity) && ((solution[std::get<0>(data.objects[i]) - 1]) != 1)) {
-            // Les deux meilleurs candidats
-            auto candidate1 = data.objects[i];
-            auto candidate2 = std::make_tuple(0, 0, 0);
-            std::cout << "best candidate : object in pos" << i << endl;
-            std::cout << "boucle sur les objets: de pos" << i + 1 << " à " << data.num_objects;
-            for (int j = i + 1; j < data.num_objects; j++) {
-                std::cout << "enter" << j;
-                if ((total_consumption + std::get<1>(data.objects[j]) <= data.capacity) && ((solution[std::get<0>(data.objects[j]) - 1]) != 1)) {
-                    candidate2 = data.objects[j];
-                    break;
-                }
-            }
-
-            std::cout << endl << endl << total_consumption << " / " << data.capacity;
-            std::cout << "\nOn CHOISIt désormais aléatoirement entre les deux meilleurs candidats d'objets : " << std::endl;
-
-            std::cout << get<0>(candidate1) << " et " << get<0>(candidate2) << endl;
-
-            tuple<int, int, int> chosen_candidate;
-            if (get<0>(candidate2) == 0) {
-                chosen_candidate = candidate1;
-            }
-            else {
-                // Choix aléatoire entre les deux candidats
-                std::default_random_engine generator(seed);
-                std::uniform_int_distribution<int> distribution(1, 2);
-                auto random_choice = distribution(generator);
-                chosen_candidate = (random_choice == 1) ? candidate1 : candidate2;
-            }
-
-
-            std::cout << "Objet choisit : " << get<0>(chosen_candidate) << endl;
-
-            // Mise à jour de la solution
-            solution[std::get<0>(chosen_candidate) - 1] = 1; // Marquage de l'objet inclus
-            total_profit += std::get<2>(chosen_candidate);
-            total_consumption += std::get<1>(chosen_candidate);
-
+        if (total_consumption + std::get<1>(data.objects[i]) <= data.capacity) {
+            solution[std::get<0>(data.objects[i]) - 1] = 1; // Marquage de l'objet inclus
+            total_profit += std::get<2>(data.objects[i]);
+            total_consumption += std::get<1>(data.objects[i]);
         }
     }
 
@@ -170,25 +133,46 @@ vector<int> solveP1(P1Data& data, int seed) {
         }
     }
     return solution;
-
+}
+// Fonction pour calculer la distance entre deux villes
+int distanceBetweenCities(const P2Data& data, int city1, int city2) {
+    return data.distances[city1][city2];
 }
 
-// Fonction pour résoudre le problème P1 avec un algorithme glouton
-int solveP2(P2Data& data, int seed) {
+// Fonction pour trouver la ville la plus proche de la dernière ville ajoutée à chaque itération dans le problème P2
+int findNearestCity(const P2Data& data, const vector<int>& visited_cities, int last_city) {
+    int nearest_city = -1;
+    int min_distance = numeric_limits<int>::max(); // initialiser avec la plus grande valeur possible
+    for (int i = 0; i < data.num_cities; i++) {
+        // Vérifier si la ville a déjà été visitée
+        if (find(visited_cities.begin(), visited_cities.end(), i) == visited_cities.end()) {
+            int distance = distanceBetweenCities(data, last_city, i);
+            // Vérifier si la ville est plus proche que la ville actuellement la plus proche
+            if (distance < min_distance) {
+                min_distance = distance;
+                nearest_city = i;
+            }
+        }
+    }
+    return nearest_city;
+}
+
+
+int solveP2(P2Data& data) {
     vector<int> lst(data.num_cities, 0);
     int total_distance = 0;
     int finish = 0;
 
     //Choix aléatoire de la ville intiale
+    std::default_random_engine generator(std::random_device{}());
     std::uniform_int_distribution<int> distribution(0, lst.size() - 1);
-    std::default_random_engine generator(seed);
     auto lastville = distribution(generator);
     vector<int> Solution = { lastville };
     lst[lastville] = 1; //On marque la ville choisie pour ne pas la réutiliser
 
     cout << "ville de départ : " << data.city_names[lastville] << " " << lastville << endl;
 
-    //Calcul des 2 meilleurs villes et choix entre les 2
+    //Calcul de la meilleure ville
     while (finish == 0) {
 
         vector<pair<int, int>> Distance;
@@ -200,36 +184,18 @@ int solveP2(P2Data& data, int seed) {
         }
         std::sort(Distance.begin(), Distance.end()); //Tri du vecteur des distances par ordre croissant
 
-        //Si il ne reste plus qu'une ville
+        //On enregistre la ville en données
+        Solution.push_back(Distance[0].second);
+        lst[Distance[0].second] = 1;
+        total_distance += Distance[0].first;
+        lastville = Distance[0].second;
+
+        //S'il ne reste plus qu'une ville
         if (Distance.size() == 1) {
-            //On enregistre la ville en données
-            cout << "Il ne reste plus qu'une ville : " << data.city_names[Distance[0].second] << " " << Distance[0].second << endl;
             finish = 1; //Fin des boucles
-            Solution.push_back(Distance[0].second);
-            lst[Distance[0].second] = 1;
-            total_distance += Distance[0].first;
-            cout << "Distance : " << Distance[0].first << endl;
-            lastville = Distance[0].second;
         }
-        else {
-            //Choix aléatoire entre les 2 villes les plus proches
-            std::uniform_int_distribution<int> distribution(0, 1);
-            std::default_random_engine generator(seed);
-            auto choosen = distribution(generator);
-
-            //Ajout de la soluton dans la liste
-            Solution.push_back(Distance[choosen].second);
-            lst[Distance[choosen].second] = 1;
-            total_distance += Distance[choosen].first;
-            lastville = Distance[choosen].second;
-
-            //Partie Affichage et déboggage
-            cout << "Distance : " << Distance[choosen].first << endl;
-            cout << "On choisit entre : " << data.city_names[Distance[0].second] << " " << Distance[0].second << " et " << data.city_names[Distance[1].second] << " " << Distance[1].second << endl;
-            cout << "La ville choisie aléatoirement est : " << data.city_names[Distance[choosen].second] << " " << Distance[choosen].second << endl;
-        }
-
     }
+
     cout << endl << "Chemin :" << endl;
 
     //On ajoute la première ville à la fin car c'est un cycle
@@ -246,40 +212,34 @@ int solveP2(P2Data& data, int seed) {
 
 
 int main(int argc, char* argv[]) {
-
     setlocale(LC_ALL, "FR_fr");
-
     // Vérifier si les noms de fichiers ont été fournis en tant que paramètres
     if (argc != 3) {
         cerr << "Deux noms de fichiers doivent être fournis en tant que paramètres." << endl;
         return 1;
     }
 
-    int seed = 3; // valeur de la graine 
-    std::default_random_engine generator(seed); // initialisation de la graine
-
     // Résolution du problème P1
     P1Data p1_data = readP1File(argv[1]);
     cout << "\nProblème P1 :" << endl;
     cout << "\nDonnées :" << endl;
     printP1Data(p1_data);
-    auto p1result = solveP1(p1_data, seed);
+    auto p1result = solveP1(p1_data);
     std::cout << "On remarque que cela n'est pas LA MEILLEURE solution car il aurait fallu prendre les objets 1, 4 et 5";
     // Résolution du problème P2
     P2Data p2_data = readP2File(argv[2]);
     cout << "\nProblème P2 :" << endl;
     cout << "\nDonnées :" << endl;
     printP2Data(p2_data);
-    cout << "Chemin trouve : " << endl;
-    int p2result = solveP2(p2_data, seed);
-    cout << "Distance : " << p2result << endl;/*
-    cout << endl << "Chemin optimal : ";
+    int p2result = solveP2(p2_data);
+    cout << "Résultat :\n" << endl;
+    cout << "Distance minimale : " << p2result << endl;
+    cout << "Chemin optimal : ";
     for (const auto& city : p2_data.city_names) {
         cout << city << " -> ";
     }
-    cout << p2_data.city_names[0] << "\n\n" << endl;*/
+    cout << p2_data.city_names[0] << "\n\n" << endl;
     return 0;
 }
-
 
 
